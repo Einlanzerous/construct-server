a1f909e4068ac7b43ea55b6570fd00b299a9c4fe
einlanzerous <einlanzerous@live.com>
2026-02-17 12:41:51 -0600

    fix: better support db init conditions

--- db/init-db.sh
+++ db/init-db.sh
@@ -6,20 +6,26 @@ set -e
 ensure_db() {
     local user=$1 pass=$2 db=$3
 
-    psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
         DO \$\$
         BEGIN
             IF NOT EXISTS (SELECT FROM pg_catalog.pg_roles WHERE rolname = '$user') THEN
                 CREATE ROLE $user WITH LOGIN PASSWORD '$pass';
+            ELSE
+                ALTER ROLE $user WITH PASSWORD '$pass';
             END IF;
         END
         \$\$;
-
-        SELECT 'CREATE DATABASE $db OWNER $user'
-        WHERE NOT EXISTS (SELECT FROM pg_database WHERE datname = '$db')\gexec
-
-        GRANT ALL PRIVILEGES ON DATABASE $db TO $user;
 EOSQL
+
+    # CREATE DATABASE cannot run inside a DO block, so check separately
+    local db_exists
+    db_exists=$(psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -tAc "SELECT 1 FROM pg_database WHERE datname = '$db'")
+    if [ "$db_exists" != "1" ]; then
+        psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "CREATE DATABASE $db OWNER $user;"
+    fi
+
+    psql --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" -c "GRANT ALL PRIVILEGES ON DATABASE $db TO $user;"
 }
 
 ensure_db vox_loop_user "$VOX_LOOP_DB_PASSWORD" vox_loop
